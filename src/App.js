05d5f09597d32d9ec82a7991caf88f991962cbcf
Mandar Ladage <mandarladage20@gmail.com>
2024-03-23 11:59:49 +0530

    Adding common scss setup and some packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './scss/common.scss'; 
+import SideNavbar from "./components/SideNavbar/Sidebar";
+import AddVoucher from "./components/AddVoucher/AddVoucher";
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <AddVoucher/>
+      {/* <SideNavbar/> */}
     </div>
   );
 }
