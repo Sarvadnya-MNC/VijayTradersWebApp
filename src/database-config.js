316e6830b4000f42b92e7a7154c8342aea47b8ac
Mandar Ladage <mandarladage20@gmail.com>
2024-03-25 18:41:38 +0530

    Adding prettier changes

--- src/database-config.js
+++ src/database-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,9 @@ const firebaseConfig = {
   storageBucket: "vijaytraders-cccdb.appspot.com",
   messagingSenderId: "960572696735",
   appId: "1:960572696735:web:fbe9290dace07c5ad35aaa",
-  measurementId: "G-2RM757VLPP"
+  measurementId: "G-2RM757VLPP",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
